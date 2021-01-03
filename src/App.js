d194d5759ca4c52f0838228716ef0e4050e788e6
astrovpls <astrov.pls@gmail.com>
2021-01-04 00:37:30 +0300

    boilerplate

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { Route, Switch } from 'react-router-dom'
+import logo from './logo.svg'
+import styles from './App.module.scss'
+
+const App = () => {
+  return (
+    <Switch>
+      <Route exact path="/"></Route>
+    </Switch>
+  )
+}
+
+export default App
